6fe7edb63420df0ac126315b6c74d2347f701180
jaydenhagerty <technocrat0505@gmail.com>
2026-02-12 16:19:57 -0800

    Additional content for the ostrich overview page

--- app/ostrichcraft/page.tsx
+++ app/ostrichcraft/page.tsx
@@ -24,8 +24,25 @@ export default function OverviewPage() {
       <h1 className="text-3xl font-bold">Overview</h1>
       <img src="/images/ostrichcraft banner.png" alt="OstrichCraft Banner" className="my-6 rounded-lg shadow-lg max-h-[400px] w-full object-cover" />
       <p>OstrichCraft is a highly-anticipated western-themed Minecraft server, season 8 in the <Link href="/" className="underline text-blue-400">FaunaCraft</Link> series. Its emphasis on cultivating a core theme with custom-developed datapacks makes it a unique entry in the series.</p>
-      <h2 className="text-2xl font-bold mt-6">Getting Started</h2>
-      <p>If you want the most immersive OstrichCraft experience, we highly recommend using our official launcher profile on Modrinth!</p>
+      <div className="flex gap-8 mt-8 flex-col lg:flex-row lg:items-start">
+        <div className="lg:max-w-[50%]">
+          <h2 id="faunacraft-enhanced" className="text-2xl font-bold mt-6">Join with FaunaCraft Enhanced</h2>
+          <p>If you want the most immersive OstrichCraft experience, we highly recommend using our official launcher profile on Modrinth!</p>
+          <br></br>
+          <p><a href="https://modrinth.com/modpack/faunacraft-enhanced" target="_blank" className="underline text-blue-400">FaunaCraft Enhanced</a> is a collection of custom-configured mods that bring immersion, performance, and quality of life improvements to your game. And it takes just 2 minutes to install!</p>
+        <details className="mt-8 border border-white/20 rounded-lg p-4">
+          <summary className="cursor-pointer font-semibold text-white/50">I'd rather use the normal launcher
+          </summary>
+          <p className="mt-4">No worries! OstrichCraft doesn't <i>require</i> this modpack, we just recommend it. You can get all the same gameplay on the standard Minecraft launcher.</p>
+          <ul className="list-disc list-inside">
+            <li>Version: 1.21.11</li>
+            <li>IP: ostrichcraft.apexmc.co</li>
+          </ul>
+        </details>
+        </div>
+        <iframe className="w-full rounded-lg aspect-video shadow-lg" src="https://www.youtube.com/embed/namxLHUqaBA"></iframe>
+      </div>
+      {/* collapsed by default accordion */}
     </>
   );
 }
